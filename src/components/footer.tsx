9e780b352725802606040199343d1e2ad55723de
SimonStnn <simon.stijnen.23@gmail.com>
2023-11-27 19:08:55 +0100

    Change footer copyright to character

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
-import { faCopyright } from "@fortawesome/free-solid-svg-icons";
 import RedirectLink from "./redirect-link";
 
 export default function Footer() {
   return (
     <footer className="flex justify-center items-center h-28 border-t bg-background text-sm text-secondary-foreground">
       <div className="flex gap-10">
-        <span>
-          <span className="sr-only">&copy;</span>
-          <FontAwesomeIcon icon={faCopyright} /> Simon 2023
-        </span>
+        <span>&copy; 2023</span>
         <span>
           <RedirectLink
             href={"https://github.com/SimonStnn"}
