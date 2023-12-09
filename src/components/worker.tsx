591c3ed06011440545a9ce802039a8acb20202c9
SimonStnn <simon.stijnen.23@gmail.com>
2023-12-09 13:01:37 +0100

    Rename setLocaleCookie function

--- src/components/worker.tsx
+++ src/components/worker.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import { useEffect } from "react";
-import { setLocaleCookies } from "@/lib/cookies";
+import { setLocaleCookie } from "@/lib/cookies";
 import { getBestMatchLocale } from "@/dictionary";
 
 export default function Worker() {
   useEffect(() => {
     const userLang = getBestMatchLocale(navigator.languages as string[]);
 
-    setLocaleCookies(userLang);
+    setLocaleCookie(userLang);
   }, []);
 
   return <></>;
--- src/lib/cookies.tsx
+++ src/lib/cookies.tsx
@@ -4,7 +4,7 @@ import { cookies } from "next/headers";
 
 const days = 60 * 60 * 24; // Seconds * minutes * hours
 
-export async function setLocaleCookies(userLang: string, force_overwrite = false) {
+export async function setLocaleCookie(userLang: string, force_overwrite = false) {
   const cookieStore = cookies();
 
   if (!force_overwrite) userLang = cookieStore.get("locale")?.value || userLang;
