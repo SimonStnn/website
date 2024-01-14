f9453cd09a17c235b72214470da04a94223f5d81
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-14 16:39:55 +0100

    Remove old project cards

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Card from "@/components/card";
 import RedirectLink from "@/components/redirect-link";
 import { getDictionary } from "@/dictionary";
 import { getLocaleCookie } from "@/lib/cookies";
@@ -12,64 +11,7 @@ export default async function Projects() {
       <h1 className="text-4xl font-bold mb">{dict.projects.title}</h1>
       <p className="my-5">{dict.projects.text.intro}</p>
       <section className="flex flex-row flex-wrap gap-5">
-        <Card
-          title={dict.projects.card.finalProject.title}
-          content={dict.projects.card.finalProject.content}
-          footer={
-            <div className="flex gap-5">
-              <span>{dict.projects.card.finalProject.footer}</span>
-              <span>
-                <RedirectLink
-                  href="https://github.com/SimonStnn/Eindwerk"
-                  addIcon={true}
-                >
-                  Github
-                </RedirectLink>
-              </span>
-            </div>
-          }
-        />
-        <Card
-          image={"/images/next-js-logo.png"}
-          title={dict.projects.card.nextjs.title}
-          content={dict.projects.card.nextjs.content}
-          footer={
-            <span className="flex flex-col items-end gap-1">
-              <RedirectLink href="https://nextjs.org/" addIcon={true}>
-                Next.js
-              </RedirectLink>
-              <RedirectLink
-                href="https://github.com/SimonStnn/website"
-                addIcon={true}
-              >
-                Github
-              </RedirectLink>
-            </span>
-          }
-        />
-        <Card
-          image={"/images/discord.svg"}
-          title={dict.projects.card.discordBot.title}
-          content={dict.projects.card.discordBot.content}
-          footer={
-            <div className="flex gap-5">
-              <span>{dict.projects.card.discordBot.footer}</span>
-              <span>
-                <RedirectLink
-                  href="https://github.com/SimonStnn/MyBot"
-                  addIcon={true}
-                >
-                  Github
-                </RedirectLink>
-              </span>
-            </div>
-          }
-        />
-        <Card
-          title={dict.projects.card.other.title}
-          content={dict.projects.card.other.content}
-          footer={dict.projects.card.other.footer}
-        />
+ 
       </section>
       <p className="my-5">{dict.projects.text.end}</p>
     </>
