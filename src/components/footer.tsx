be8adb4b0ee205ffb01a100688d3b47c3c61956f
SimonStnn <simon.stijnen.23@gmail.com>
2024-10-08 18:10:25 +0200

    Update footer with dynamic year and resizable for smaller screens

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,37 +7,38 @@ import { Separator } from "./ui/separator";
 
 export default function Footer() {
   return (
-    <footer className="flex justify-center items-center h-28 border-t bg-background text-sm text-secondary-foreground">
-      <div className="flex gap-6">
-        <span>&copy; 2023</span>
-          <Separator orientation="vertical" />
-        <span>
-          <RedirectLink
-            href={"https://github.com/SimonStnn"}
-            className="flex gap-2 text-sm"
-          >
-            <FontAwesomeIcon icon={faGithub} size="lg" />
-            Github
-          </RedirectLink>
-        </span>
-        <Separator orientation="vertical" />
-        <span>
-          <RedirectLink
-            href={"https://www.linkedin.com/in/simon-stijnen/"}
-            className="flex gap-2 text-sm"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="lg" />
-            LinkedIn
-          </RedirectLink>
-        </span>
-        <Separator orientation="vertical" />
-        <span>
-          <RedirectLink href={"mailto:simon@stijnen.be"} className="flex gap-2 text-sm">
-            <FontAwesomeIcon icon={faEnvelope} size="lg" />
-            simon@stijnen.be
-          </RedirectLink>
-        </span>
-      </div>
+    <footer className="flex flex-col sm:flex-row justify-center items-start sm:items-center gap-6 p-10 border-t bg-background text-sm text-secondary-foreground">
+      <span>&copy; 2023 &minus; {new Date().getFullYear()}</span>
+      <Separator orientation="vertical" className="hidden sm:inline-block" />
+      <span>
+        <RedirectLink
+          href={"https://github.com/SimonStnn"}
+          className="flex gap-2 text-sm"
+        >
+          <FontAwesomeIcon icon={faGithub} size="lg" />
+          Github
+        </RedirectLink>
+      </span>
+      <Separator orientation="vertical" className="hidden sm:inline-block" />
+      <span>
+        <RedirectLink
+          href={"https://www.linkedin.com/in/simon-stijnen/"}
+          className="flex gap-2 text-sm"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="lg" />
+          LinkedIn
+        </RedirectLink>
+      </span>
+      <Separator orientation="vertical" className="hidden sm:inline-block" />
+      <span>
+        <RedirectLink
+          href={"mailto:simon@stijnen.be"}
+          className="flex gap-2 text-sm"
+        >
+          <FontAwesomeIcon icon={faEnvelope} size="lg" />
+          simon@stijnen.be
+        </RedirectLink>
+      </span>
     </footer>
   );
 }
