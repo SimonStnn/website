db7ef67f4e898285a8dccb8babe2b1c433e83599
SimonStnn <simon.stijnen.23@gmail.com>
2026-02-08 19:44:05 +0000

    Add `robots.txt`

---
+++ app/robots.ts
@@ -0,0 +1,16 @@
+import type { MetadataRoute } from "next";
+
+import { siteConfig } from "@/lib/config";
+
+export default function robots(): MetadataRoute.Robots {
+  const sitemapUrl = new URL("/sitemap.xml", siteConfig.url).toString();
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: sitemapUrl,
+    host: siteConfig.url,
+  };
+}
